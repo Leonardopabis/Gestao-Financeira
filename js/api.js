852b63cf74d6e7009d62a1a5f03c94d0ecb304f9
leofpabis <leofilipake@gmail.com>
2026-07-17 12:46:32 -0300

    arrumado a criacao de novas transactions e arrumado como é mostrado a lista

--- js/api.js
+++ js/api.js
@@ -1,3 +1,4 @@
+// Criar estrutura html da transaçãox
 const criarItemHtml = (descricao, categoria, tipo, valor, data, receita) => {
     const classeTipo = receita ? 'income' : 'expense'
     const classeValor = receita ? 'income-value' : 'expense-value'
@@ -5,10 +6,11 @@ const criarItemHtml = (descricao, categoria, tipo, valor, data, receita) => {
     const dataFormatada = formartarData(data)
     let imagem = ''
     let descricaoImagem = ''
+    let categoriaFormatada = categoria.toLowerCase()
     if (categoria.toLowerCase() === 'trabalho') {
         imagem = './img/filter-cifrao.png'
         descricaoImagem = 'ícone de cifrão referente à receita'
-    }
+    } else if (cate)
     return `
                             <tr>
                                 <td>
@@ -57,6 +59,7 @@ const criarItemHtml = (descricao, categoria, tipo, valor, data, receita) => {
 `
 }
 
+// Carregar dados do sql na lista
 async function carregarTransacoes() {
     try {
         const resposta = await fetch('http://localhost:3000/api/get-transactions')
@@ -68,14 +71,21 @@ async function carregarTransacoes() {
         const dados = await resposta.json()
 
         const listaDeTransacoes = document.querySelector('.transaction-list-body')
+        listaDeTransacoes.innerHTML = null
         dados.forEach(item => {
             let isReceita = null
-            if (item.tipo === 'Receita') {
+            if (item.tipo.toLowerCase() === 'receita') {
                 isReceita = true
+                item.tipo = 'Receita'
             } else {
                 isReceita = false
+                item.tipo = 'Despesa'
             }
-            const novaTransacao = criarItemHtml(item.descricao, item.categoria, item.tipo, item.valor, item.data, isReceita)
+
+            const formatoBrasileiro = new Intl.NumberFormat('pt-br', { minimumFractionDigits: 2, maximumFractionDigits: 2})
+            const valorFormatado = formatoBrasileiro.format(item.valor)
+
+            const novaTransacao = criarItemHtml(item.descricao, item.categoria, item.tipo, valorFormatado, item.data, isReceita)
             listaDeTransacoes.insertAdjacentHTML('afterbegin', novaTransacao)
         });
     } catch (erro) {
@@ -90,3 +100,57 @@ function formartarData(dataISO) {
 }
 
 carregarTransacoes()
+
+// Criar nova transação
+const formularioNovaTransacao = document.querySelector('#new-transaction-form')
+
+formularioNovaTransacao.addEventListener('submit', async (event) => {
+    event.preventDefault()
+    
+    const descricaoHtml = document.querySelector('#description').value
+    const valorHtml = document.querySelector('#value').value
+    const categoriaHtml = document.querySelector('#categories').value
+    const tipoSelecionado = document.querySelector('input[name="type"]:checked')
+    const dataHtml = document.querySelector('#transaction-date').value
+    
+    const valor = Number(valorHtml.replace(/\./g, '').replace(',', '.'))
+    
+    if (!tipoSelecionado) {
+        alert('Selecione Receita ou Despesa')
+        return
+    }
+
+    const tipoHtml = tipoSelecionado.value;
+
+    if (Number.isNaN(valor)){
+        alert('Digite um valor válido')
+        return
+    }
+
+    const dadosForm = {
+        descricao: descricaoHtml,
+        categoria: categoriaHtml,
+        tipo: tipoHtml,
+        valor: valor,
+        data: dataHtml
+    }
+
+    try {
+        const resposta = await fetch('http://localhost:3000/api/add-new-transaction', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(dadosForm),
+        })
+        const resultado = await resposta.json()
+        console.log('Resposta do servidor: ', resultado)
+        console.log('dados enviados')
+        carregarTransacoes()
+        formularioNovaTransacao.reset()
+
+    } catch (error) {
+        console.log('Erro ao enviar: ', error)
+    }
+
+})
